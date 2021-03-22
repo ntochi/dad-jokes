07dc32b6914bbfb0d2eeef7810d4e2b0e536b3e2
Nancy <nancytochi24@gmail.com>
2021-03-22 16:22:53 -0400

    style changes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// * 1) DAD JOKE
 const jokes = document.querySelector('#jokes');
 const button = document.querySelector('button');
 
@@ -22,36 +21,3 @@ const getDadJoke = async () => {
 
 button.addEventListener('click', addNewJoke)
 
-
-
-// // * 2) BITCOIN PRICE
-// const fetchBitcoinPrice = async () => {
-//     try {
-//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-//         console.log(res.data.ticker.price)
-//     } catch (e) {
-//         console.log("ERROR!", e)
-//     }
-// }
-
-
-// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-//     .then(res => {
-//         console.log("RESPONSE, WAITING TO PARSE...", res)
-//         return res.json()
-//     })
-//     .then(data => {
-//         console.log("DATA PARSED...")
-//         console.log(data.ticker.price)
-//     })
-//     .catch(e => {
-//         console.log("OH NO! ERROR!", e)
-//     })
-
-// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-//     .then(res => {
-//         console.log(res.data.ticker.price)
-//     })
-//     .catch(err => {
-//         console.log("ERROR!", err)
-//     })
